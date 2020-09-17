d349a47201acae0a6125ba9cd68e2727a9f47eca
Adren <35825183+IamAdren@users.noreply.github.com>
2020-09-17 00:27:20 -0400

    Update fxmanifest.lua

--- FiveM Script/fxmanifest.lua
+++ FiveM Script/fxmanifest.lua
@@ -1,12 +1,9 @@
 fx_version 'adamant'
-
 game 'gta5'
-
-author 'Sheriff Matt'
-
+author 'IamAdren'
 description 'Toll Booth'
-
 version '2.0.0'
+this_is_a_map 'yes'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
